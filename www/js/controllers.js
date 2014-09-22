4134a27a4d58c3dce1738c828d7c65037dbd2948
tiantianquan <fenqiufen@gmail.com>
2014-09-22 09:51:27 +0800

    commit

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope) {
+})
+
+.controller('SellCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
