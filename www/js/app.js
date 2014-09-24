12f9a1f90b5d7faaf3e367d09af7ec7ada4b1357
tiantianquan <fenqiufen@gmail.com>
2014-09-24 17:32:51 +0800

    commit

--- www/js/app.js
+++ www/js/app.js
@@ -7,27 +7,33 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
+     //搜索页(sug:放在tab前面才能识别
+    .state('search', {
+      url: '/search',
+      templateUrl: 'templates/search.html',
+      controller: 'SearchCtrl'
+    })
 
     // setup an abstract state for the tabs directive
     .state('tab', {
@@ -37,8 +43,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     })
 
     // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
+    .state('tab.home', {
       url: '/home',
       views: {
         'tab-home': {
@@ -77,12 +82,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
 
-    //搜索页
-    .state('search',{
-      url:'/search',
-      templateUrl: 'templates/search.html',
-      controller:'SearchCtrl'
-    })
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,21 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {
+.controller('HomeCtrl', function ($scope) {
 })
 
-.controller('SellCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('SellCtrl', function ($scope, Friends) {
+
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function ($scope) {
 })
 
-.controller('SearchCtrl', function($scope){
+.controller('SearchCtrl', function ($scope,$ionicNavBarDelegate) {
+  $scope.goBack = function () {
+    $ionicNavBarDelegate.back();
+  };
 });
