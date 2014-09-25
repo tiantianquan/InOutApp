476df59e84379fc330604f1ea2d3477d6ec09f71
tiantianquan <fenqiufen@gmail.com>
2014-09-25 22:50:37 +0800

    增加下部插入

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,29 +1,52 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function ($scope,$location) {
+.controller('HomeCtrl', function($scope, $location, $ionicModal) {
   $scope.$root.searchShow = true;
-  $scope.$on('$destroy',function(){
+  $scope.$on('$destroy', function() {
     $scope.$root.searchShow = false;
   })
 
-  $scope.click = function(){
-    $location.path('tab/home/product/2')
-  }
-})
-
-.controller('SellCtrl', function ($scope, Friends) {
 
 })
 
-.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+.controller('SellCtrl', function($scope, Friends) {
+
 })
 
-.controller('AccountCtrl', function ($scope) {
-})
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {})
 
-.controller('SearchCtrl', function ($scope) {
-})
+.controller('AccountCtrl', function($scope) {})
 
-.controller('ProductItemCtrl',function($scope,$stateParams, ProductItems){
+.controller('SearchCtrl', function($scope) {})
+
+.controller('ProductItemCtrl', function($scope, $stateParams, ProductItems) {
   $scope.productItem = ProductItems.get($stateParams.productItemId);
+})
+
+.controller('TabCtrl', function($scope, $ionicModal) {
+
+  $ionicModal.fromTemplateUrl('templates/tab-sell.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 });
